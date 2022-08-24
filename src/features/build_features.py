4023517c7d20d0feef08a4ff7a7d76f984daa4a1
roshankumar2303 <roshankumar2109@gmail.com>
2022-08-25 01:28:53 +0530

    Added RFE in src/features/build_features.py

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,7 +1,7 @@
 import sys, os
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import RFE, SelectKBest, f_classif
 
 # ------------------------------------------------------------
 # Adding CWD to path, for local module imports
@@ -18,6 +18,22 @@ def select_k_best_features(X: pd.DataFrame, Y: pd.DataFrame):
     k_best = SelectKBest(score_func=f_classif, k=50)
     k_best.fit(X, np.ravel(Y))
     features_list = list(X.columns[k_best.get_support()])
+    for column in X:
+        if column not in features_list:
+            X.drop(columns=column, inplace=True)
+
+    # Returning the list of features, modified X and Y sets
+    return features_list, X, Y
+
+
+def recursive_feature_elimination(X: pd.DataFrame, Y: pd.DataFrame, estimator):
+    # Cleaning the Data
+    X, Y = clean_data(X, Y)
+
+    # Feature selection using RFE
+    rfe_selector = RFE(estimator=estimator, n_features_to_select=50, step=1)
+    rfe_selector.fit(X, np.ravel(Y))
+    features_list = list(X.columns[rfe_selector.get_support()])
     for column in X:
         if column not in features_list:
             X.drop(columns=column, inplace=True)
